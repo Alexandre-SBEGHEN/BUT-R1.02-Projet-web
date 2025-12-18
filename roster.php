74627efc5eae2d4ff65db0437d9b8c9fb5ba691e
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2025-12-18 17:07:44 +0100

    Ajout balises title et favicon

--- roster.php
+++ roster.php
@@ -5,8 +5,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php echo $translations->$lang_current->pages->roster->meta_desc; ?>">
-        <link rel="icon" type="image/png" href="favicon.png">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" type="image/png" href="favicon.png">
         <link rel="stylesheet" href="css/style.css">
         <title><?php $docname=get_page_name(); echo $translations->$lang_current->docname->$docname; ?></title>
     </head>
--- roster_card.php
+++ roster_card.php
@@ -5,8 +5,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="<?php echo 'This is the page dedicated to the ', $cards->$name->name, ' Brainrot character. Learn more about it.' ?>">
-        <link rel="icon" type="image/png" href="favicon.png">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" type="image/png" href="favicon.png">
         <link rel="stylesheet" href="css/style.css">
         <title><?php echo $cards->$name->name, ' - Brainrot City'; ?></title>
     </head>
